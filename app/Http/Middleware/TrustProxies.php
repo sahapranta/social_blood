90af2ec2b23b0de9310349777b24760f83e244c1
Pranta Saha <pranta1204@gmail.com>
2019-09-09 14:32:58 +0600

    https: trusted proxy added as *

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,12 +12,13 @@ class TrustProxies extends Middleware
      *
      * @var array|string
      */
-    protected $proxies;
+    protected $proxies = *;
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+//     protected $headers = Request::HEADER_X_FORWARDED_ALL;
+        protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
 }
